71535de5f48f7d053811ec97aaff3de20eab2aa0
Narvaez0993 <61300610+Narvaez0993@users.noreply.github.com>
2024-03-19 12:02:59 -0500

    feat(routes-and-sidebar): routes-and-sidebar (#1) (#2)
    The sidebar and corresponding routes are added
    
    FEAT https://cosmic-libra-cdd.notion.site/StarWars-Universe-2aa640a06b91460f9246beeb75e0d980?p=2cc2a895ca624ae19c649d6cbd0e85d7&pm=s

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  RouterProvider,
+} from "react-router-dom"
+import Router from './shared/infraestructure/Router.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router}/>
   </React.StrictMode>,
 )
